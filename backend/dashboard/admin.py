c50ace1b1ddf5701df59816c8c6c428b13ebcbec
RoyalAagman <aagman.iaf.chandra@gmail.com>
2022-02-07 10:18:33 +0530

    model created in dashboard

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Upload
 
-# Register your models here.
+
+class UploadAdmin(admin.ModelAdmin):
+    list_display = ('file', 'uploaded_at')
+
+
+admin.site.register(Upload, UploadAdmin)

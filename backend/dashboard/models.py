8738faeab2178b00c0eca4b69919afcd18c88785
RoyalAagman <aagman.iaf.chandra@gmail.com>
2022-02-11 00:02:35 +0530

    models changes

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -9,7 +9,7 @@ class Upload(models.Model):
         return self.file.name
 
 
-def handle_uploaded_file(f):
-    with open('some/file/name.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
+# def handle_uploaded_file(f):
+#     with open('some/file/name.txt', 'wb+') as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)

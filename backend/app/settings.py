3fd9c010f22b74daaffcbf875918732e5e82e7a6
RoyalAagman <aagman.iaf.chandra@gmail.com>
2022-02-10 23:25:16 +0530

    zipfiles saving in folder uploads

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
-    'dashboard'
+    'dashboard',
+    'zipfile'
 ]
 
 MIDDLEWARE = [
--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django_zipfile import TemplateZipFile
 from django.views.generic import FormView
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from .forms import UploadZipFileForm
-from .models import handle_uploaded_file
+from .models import Upload
 
 
 class UploadView(FormView):
@@ -11,25 +9,8 @@ class UploadView(FormView):
     form_class = UploadZipFileForm
     success_url = '/'
 
-    def upload_file(request):
-        if request.method == 'POST':
-            form = UploadZipFileForm(request.POST, request.FILES)
-            if form.is_valid():
-                handle_uploaded_file(request.FILES['file'])
-                return HttpResponseRedirect('/success/url/')
-        else:
-            form = UploadZipFileForm()
-        return render(request, 'upload.html', {'form': form})
-
-
-def gen_zip(pk, name, vars):
-    zipObj = ZipFile(os.path.join(
-        '/tmp/', str(name) + '_' + str(pk) + '.zip'), 'w')
-    zipObj.write(pdf_files[0].path, '/filea.pdf')
-    zipObj.write(pdf_files[1].path, '/fileb.pdf')
-
-
-def aksorder_complete(request, pk):
-    ao = get_object_or_404(AksOrder, id=pk)
-    zipObj = generate_shop_zip(ao.c.pk, ao.dl, ao.vars)
-    ao.zip_file.save('file.zip', zipObj)
+    def form_valid(self, form):
+        new_object = Upload.objects.create(
+            file=form.cleaned_data['file']
+        )
+        return super(UploadView, self).form_valid(form)

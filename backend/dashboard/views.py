8b0e414a64cafe074374a9e0c0c24bd79d21ef25
RoyalAagman <aagman.iaf.chandra@gmail.com>
2022-02-10 22:34:48 +0530

    new function added in views

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -20,3 +20,16 @@ class UploadView(FormView):
         else:
             form = UploadZipFileForm()
         return render(request, 'upload.html', {'form': form})
+
+
+def gen_zip(pk, name, vars):
+    zipObj = ZipFile(os.path.join(
+        '/tmp/', str(name) + '_' + str(pk) + '.zip'), 'w')
+    zipObj.write(pdf_files[0].path, '/filea.pdf')
+    zipObj.write(pdf_files[1].path, '/fileb.pdf')
+
+
+def aksorder_complete(request, pk):
+    ao = get_object_or_404(AksOrder, id=pk)
+    zipObj = generate_shop_zip(ao.c.pk, ao.dl, ao.vars)
+    ao.zip_file.save('file.zip', zipObj)
